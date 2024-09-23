873ec89e54fe76c138bd2d1ad5014acb2b308328
miroshx <tabletkovv@mail.ru>
2024-09-23 13:14:34 +0300

    1

--- LR1/Program.cs
+++ LR1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!1");
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод и обработка трех аргументов
+        for (int i = 1; i <= 3; i++)
+        {
+            Console.Write($"Введите значение аргумента {i}: ");
+            if (double.TryParse(Console.ReadLine(), out double x))
+            {
+                // Вычисление f(x) = x^2 - ln|x + 1| - 3
+                double result = Math.Pow(x, 2) - Math.Log(Math.Abs(x + 1)) - 3;
+
+                // Округление до 2 знаков после запятой и вывод результата
+                Console.WriteLine($"f({x}) = {Math.Round(result, 2)}");
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод.");
+            }
+        }
+    }
+}
--- LR2/Program.cs
+++ LR2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!2");
+﻿
+class Program
+{
+    static void Main()
+    {
+        int count = 0;
+
+        // Чтение трех предложений от пользователя
+        for (int i = 1; i <= 3; i++)
+        {
+            Console.WriteLine($"Введите предложение {i}:");
+            string sentence = Console.ReadLine();
+
+            // Разделение строки на слова
+            string[] words = sentence.Split(new char[] { ' ', ',', '.', '!', '?', ';', ':' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Подсчет слов с длиной <= 4 символов
+            foreach (string word in words)
+            {
+                if (word.Length <= 4)
+                {
+                    count++;
+                }
+            }
+        }
+
+        // Вывод результата
+        Console.WriteLine($"Количество слов длиной не более 4 букв: {count}");
+    }
+}
